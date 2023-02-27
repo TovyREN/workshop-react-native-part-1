de98c01b541a3135885de51ce0f6b8de324c59de
Tovy <tovy.ren@epitech.eu>
2023-02-27 15:04:37 +0100

    [ADD] useeffect but not used

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
-import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Image } from 'react-native';
 import Constants from 'expo-constants';
+import React, { useState, useEffect } from 'react';
 
 export default function App() {
+  useEffect(() => {    // Update the document title using the browser API
+    //document.title = `You clicked ${count} times`;
+  }, []);
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        source={{
+          uri: 'https://reactnative.dev/docs/assets/p_cat1.png',
+        }}
+        style={{width: 200, height: 200}}
+      />
+      <Text>Hello, I am your cat!</Text>
     </View>
   );
 }
@@ -13,7 +23,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#E2E6E1',
+    backgroundColor:'#E2E6E1',
     alignItems: 'center',
     paddingTop: Constants.statusBarHeight,
     justifyContent: 'center',
